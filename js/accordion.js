559ba44e06935bc1b1beb615d8cec39ada654c78
Andrew Lagutin <iamroypchel@gmail.com>
2022-08-29 17:53:30 +0300

    change accordion, fix layout

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,9 @@
-$(function () {
-  $("#accordion").accordion({
-    active: true,
-    collapsible: true,
+document.addEventListener("DOMContentLoaded", function () {
+  let acc = new Accordion(".faq__list", {
+    duration: 700,
+    elementClass: "faq__item",
+    triggerClass: "faq__top",
+    panelClass: "faq__bottom",
+    showMultiple: false,
   });
 });
