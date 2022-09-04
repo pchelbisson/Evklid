d3a75ab07698fa3cf4a0a0e509165847c5ef9b4d
Andrew Lagutin <iamroypchel@gmail.com>
2022-09-04 21:37:56 +0300

    fix ally and add custom checkbox

--- js/swiper.js
+++ js/swiper.js
@@ -4,4 +4,7 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  a11y: {
+    paginationBulletMessage: "Проектные решения любой сложности {{index}}",
+  },
 });
